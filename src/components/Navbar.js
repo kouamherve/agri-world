47c09136af47a781f5bf1dd3664219c4fa5e2970
Herve Kouam <kouamherve01@gmail.com>
2024-05-21 22:00:13 -0400

    🚧  F01: APP > Navbar > Add style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import React from "react";
+import { CiMenuBurger } from "react-icons/ci";
 
 export default function Navbar() {
   return (
-    <div className=" p-4 flex items-center justify-between">
-      <div>logo</div>
-      <div>menu</div>
+    <div className=" px-4 py-2 flex items-center justify-between font-Montserrat shadow">
+      <div>
+        <h1 className=" text-2xl font-bold">
+          Agri-<span className=" text-green-600">world</span>
+        </h1>
+      </div>
+      <div className="p-2 text-3xl ">
+        <CiMenuBurger />
+      </div>
     </div>
   );
 }
