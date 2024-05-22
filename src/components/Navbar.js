6e9e2c3ba2b726514d5a55f9a1eb4215a7c6f1e8
Herve Kouam <kouamherve01@gmail.com>
2024-05-21 22:38:41 -0400

    🚧  F01: APP > Navbar > Add interaction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [isShow, setIsShow] = useState(false);
+
   return (
-    <div className=" px-4 py-2 flex items-center justify-between font-Montserrat shadow">
+    <nav className=" fixed px-4 py-1 bg-white font-Montserrat w-full flex flex-wrap items-center justify-between  shadow overflow-x-hidden">
       <div>
-        <h1 className=" text-2xl font-bold">
-          Agri-<span className=" text-green-600">world</span>
-        </h1>
+        <Link to="#">
+          <h1 className=" text-2xl font-bold">
+            Agri-<span className=" text-green-600">world</span>
+          </h1>
+        </Link>
       </div>
-      <div className="p-2 text-3xl ">
-        <CiMenuBurger />
+      <div
+        onClick={() => setIsShow(!isShow)}
+        className="p-2 text-3xl cursor-pointer fill-current "
+      >
+        {isShow ? <AiOutlineClose /> : <CiMenuBurger />}
       </div>
-    </div>
+      <div className={`${isShow ? "block" : "hidden"} w-full p-4 font-bold`}>
+        <ul className="flex flex-col items-center gap-4">
+          <Link>
+            <li>Home</li>
+          </Link>
+          <Link>
+            <li>About</li>
+          </Link>
+          <Link>
+            <li>Services</li>
+          </Link>
+          <Link>
+            <li>Gallery</li>
+          </Link>
+          <Link>
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
   );
 }
