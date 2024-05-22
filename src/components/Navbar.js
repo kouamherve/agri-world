62ea38cf1b72ab941b6a227a212a70e8ff666a19
Herve Kouam <kouamherve01@gmail.com>
2024-05-21 21:48:01 -0400

    ✨ F01: APP > Navbar > Add layout

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className=" p-4 flex items-center justify-between">
+      <div>logo</div>
+      <div>menu</div>
+    </div>
+  );
+}
