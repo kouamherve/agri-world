f558a16abcea0a6b21cb6b39769d09e813bc8690
Herve Kouam <kouamherve01@gmail.com>
2024-05-27 19:36:13 -0400

    🚧  F02: APP > Homepage > WhatWeDo > Add SM breakpoint

--- src/components/homepage/WhatWeDo.js
+++ src/components/homepage/WhatWeDo.js
@@ -3,16 +3,18 @@ import Check from "../../reusable-ui/Check";
 export default function WhatWeDo() {
   return (
     <div className=" pt-14 pb-14">
-      <div className=" mx-4 h-72 w-fit max-[320px]:h-64 max-[425px]:h-80    ">
+      <div className=" mx-4 h-72 w-fit max-[320px]:h-64 max-[425px]:h-80 sm:mx-16 sm:h-96">
         <img
           className=" object-cover object-center w-full h-full rounded-lg"
           src="https://cdn.pixabay.com/photo/2018/10/03/21/57/cabbage-3722498_1280.jpg"
           alt=""
         />
       </div>
-      <div className=" mx-4 pt-14 font-Montserrat capitalize">
-        <h4 className=" text-sm text-primary font-medium">What we do</h4>
-        <h1 className=" text-[28px] font-bold mt-0 max-[320px]:text-[26px] max-[425px]:text-3xl">
+      <div className=" mx-4 pt-14 font-Montserrat capitalize sm:mx-16">
+        <h4 className=" text-sm text-primary font-medium sm:text-base">
+          What we do
+        </h4>
+        <h1 className=" text-[28px] font-bold mt-0 max-[320px]:text-[26px] max-[425px]:text-3xl sm:text-4xl">
           What we offer for you
         </h1>
         <p className=" text-gray-500 mt-2 mb-8 max-[320px]:text-justify">
