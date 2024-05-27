beceab0937038ff50509273d505b14d1245a3f82
Herve Kouam <kouamherve01@gmail.com>
2024-05-26 22:27:25 -0400

    🚧  F02: APP > Homepage > WhatWeDo > Add 320px breakpoint

--- src/components/homepage/WhatWeDo.js
+++ src/components/homepage/WhatWeDo.js
@@ -3,7 +3,7 @@ import Check from "../../reusable-ui/Check";
 export default function WhatWeDo() {
   return (
     <div className=" pt-14 pb-14">
-      <div className=" mx-4 h-80 w-fit">
+      <div className=" mx-4 h-72 w-fit max-[320px]:h-64">
         <img
           className=" object-cover object-center w-full h-full rounded-lg"
           src="https://cdn.pixabay.com/photo/2018/10/03/21/57/cabbage-3722498_1280.jpg"
@@ -11,13 +11,15 @@ export default function WhatWeDo() {
         />
       </div>
       <div className=" mx-4 pt-14 font-Montserrat capitalize">
-        <h4 className=" text-sm text-green-600 font-medium">What we do</h4>
-        <h1 className=" text-[28px] font-bold mt-0">What we offer for you</h1>
-        <p className=" text-gray-500 mt-2 mb-8 ">
+        <h4 className=" text-sm text-primary font-medium">What we do</h4>
+        <h1 className=" text-[28px] font-bold mt-0 max-[320px]:text-[26px]">
+          What we offer for you
+        </h1>
+        <p className=" text-gray-500 mt-2 mb-8 max-[320px]:text-justify">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, dicta
           laboriosam. Deserunt fugiat accusantium explicabo excepturi.
         </p>
-        <div className=" space-y-4">
+        <div className=" space-y-4 max-[320px]:space-y-3">
           <Check service="weed control" />
           <Check service="pest control" />
           <Check service="grass sedding" />
--- src/reusable-ui/Check.js
+++ src/reusable-ui/Check.js
@@ -2,11 +2,13 @@ import { GiCheckMark } from "react-icons/gi";
 
 export default function Check({ service }) {
   return (
-    <div className="flex items-center gap-4">
-      <div className=" bg-violet-100/50 p-2 size-8 rounded-full">
-        <GiCheckMark className=" text-sm" />
+    <div className="flex items-center gap-3">
+      <div className=" bg-secondary p-2 size-8 rounded-full max-[320px]:size-6">
+        <GiCheckMark className=" text-sm max-[320px]:text-xs" />
       </div>
-      <span className=" text-base font-bold ">{service}</span>
+      <span className=" text-base font-bold max-[320px]:text-[15px]">
+        {service}
+      </span>
     </div>
   );
 }
