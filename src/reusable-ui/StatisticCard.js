6c00acf02f6df8183fa49fd191421644370c853f
Herve Kouam <kouamherve01@gmail.com>
2024-05-26 22:28:38 -0400

    🚧  F02: APP > Homepage > Statistic > Add 320px breakpoint

--- src/reusable-ui/StatisticCard.js
+++ src/reusable-ui/StatisticCard.js
@@ -3,11 +3,9 @@ import React from "react";
 export default function StatisticCard({ number, title }) {
   return (
     <div className=" font-Montserrat mx-4">
-      <div className="text-center">
-        <span className=" text-green-600 text-3xl font-extrabold">
-          {number}
-        </span>
-        <h2 className=" text-gray-500 text-xl capitalize font-light text-nowrap">
+      <div className="text-center max-[320px]:text-start ">
+        <span className=" text-primary text-3xl font-extrabold">{number}</span>
+        <h2 className=" text-gray-500 mt-3 text-xl capitalize font-light text-nowrap max-[320px]:text-lg max-[320px]:text-wrap max-[320px]:pr-10">
           {title}
         </h2>
       </div>
