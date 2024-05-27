aca9683bacbeab35f1895fb15b71b569df98102f
Herve Kouam <kouamherve01@gmail.com>
2024-05-27 19:34:23 -0400

    🚧  F02: APP > Navbar > Add SM breakpoint

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -10,10 +10,10 @@ export default function Navbar() {
   const [isShow, setIsShow] = useState(false);
 
   return (
-    <nav className=" z-50 fixed top-0 left-0 right-0 px-4 py-3 bg-white font-Montserrat w-full flex flex-wrap items-center justify-between shadow overflow-x-hidden md:px-10 md:py-4 lg:flex-nowrap lg:gap-14 lg:px-12 xl:px-36 2xl:px-52">
+    <nav className="z-50 fixed top-0 left-0 right-0 px-4 py-3 bg-white font-Montserrat w-full flex flex-wrap items-center justify-between shadow overflow-x-hidden sm:px-16 sm:py-5 lg:flex-nowrap lg:gap-14 lg:px-12 xl:px-36 2xl:px-52">
       <div>
         <Link to="#">
-          <h1 className=" text-2xl text-black font-bold md:text-3xl lg:text-nowrap">
+          <h1 className=" text-2xl text-black font-bold sm:text-3xl lg:text-nowrap">
             Agri-<span className=" text-primary">world</span>
           </h1>
         </Link>
@@ -22,7 +22,7 @@ export default function Navbar() {
         onClick={() => setIsShow(!isShow)}
         className="text-[34px] text-gray-700 cursor-pointer flex items-center gap-5 md:text-4xl lg:hidden"
       >
-        <MdDarkMode className=" text-2xl md:text-[26px]" />
+        <MdDarkMode className=" text-2xl md:text-[26px] p-0" />
         {isShow ? <AiOutlineClose /> : <IoMenu />}
       </div>
       <Menu isShow={isShow} />
