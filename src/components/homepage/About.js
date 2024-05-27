60da3240ba0934b9033c2a3a055c79630c76ec62
Herve Kouam <kouamherve01@gmail.com>
2024-05-27 19:35:55 -0400

    🚧  F02: APP > Homepage > About > Add SM breakpoint

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -4,10 +4,12 @@ import Button from "../../reusable-ui/Button";
 export default function About() {
   return (
     <div className=" bg-secondary py-14">
-      <div className=" mx-4 pb-8 font-Montserrat capitalize">
+      <div className=" mx-4 pb-8 font-Montserrat capitalize sm:mx-16">
         <div>
-          <h4 className=" text-sm text-primary font-medium">About us</h4>
-          <h1 className=" text-[28px] font-bold max-[320px]:text-[26px] max-[425px]:text-3xl">
+          <h4 className=" text-sm text-primary font-medium sm:text-base">
+            About us
+          </h4>
+          <h1 className=" text-[28px] font-bold max-[320px]:text-[26px] max-[425px]:text-3xl sm:text-4xl">
             A new way to invest in agriculture
           </h1>
         </div>
@@ -18,11 +20,11 @@ export default function About() {
           praesentium quo fugit deleniti quam eveniet. Veritatis, eius veniam
           soluta in placeat dolorem.
         </p>
-        <Button className="bg-primary text-white hover:text-primary border border-primary px-8 py-3.5 mb-4 text-base max-[425px]:px-10">
+        <Button className="bg-primary text-white hover:text-primary border border-primary px-8 py-3.5 mb-4 text-base max-[425px]:px-10 sm:px-9">
           Read more
         </Button>
       </div>
-      <div className=" mx-4 h-72 w-fit max-[320px]:h-64 max-[425px]:h-80 ">
+      <div className=" mx-4 h-72 w-auto max-[320px]:h-64 max-[425px]:h-80 sm:mx-16 sm:h-96">
         <img
           className=" object-cover object-center w-full h-full rounded-lg"
           src="https://cdn.pixabay.com/photo/2021/05/03/13/32/greenhouse-6226263_1280.jpg"
