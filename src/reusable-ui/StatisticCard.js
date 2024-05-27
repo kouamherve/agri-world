e3440f9d1c7a73e9bebf3ac39127cd96bb7d52a8
Herve Kouam <kouamherve01@gmail.com>
2024-05-27 08:27:13 -0400

    🚧  F02: APP > Homepage > Statistic > Add 425px breakpoint

--- src/reusable-ui/StatisticCard.js
+++ src/reusable-ui/StatisticCard.js
@@ -4,8 +4,10 @@ export default function StatisticCard({ number, title }) {
   return (
     <div className=" font-Montserrat mx-4">
       <div className="text-center max-[320px]:text-start ">
-        <span className=" text-primary text-3xl font-extrabold">{number}</span>
-        <h2 className=" text-gray-500 mt-3 text-xl capitalize font-light text-nowrap max-[320px]:text-lg max-[320px]:text-wrap max-[320px]:pr-10">
+        <span className=" text-primary text-3xl font-extrabold max-[425px]:text-[36px]">
+          {number}
+        </span>
+        <h2 className=" text-gray-500 mt-3 text-xl capitalize font-light text-nowrap max-[320px]:text-lg max-[320px]:text-wrap max-[320px]:pr-10 max-[425px]:text-lg">
           {title}
         </h2>
       </div>
