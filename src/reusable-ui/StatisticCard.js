3f335780669dec619a4994850407125c51fcbd13
Herve Kouam <kouamherve01@gmail.com>
2024-05-28 23:06:39 -0400

    🚧  F03: APP > Homepage > Statistic > Add SM breakpoint

--- src/reusable-ui/StatisticCard.js
+++ src/reusable-ui/StatisticCard.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function StatisticCard({ number, title }) {
   return (
-    <div className=" font-Montserrat mx-4">
+    <div className=" font-Montserrat mx-4 sm:mx-16">
       <div className="text-center ">
         <span className=" text-primary text-4xl font-extrabold">{number}</span>
-        <h2 className=" text-gray-800 mt-3 text-lg capitalize font-light ">
+        <h2 className=" text-gray-800 mt-3 text-lg capitalize font-light sm:text-xl">
           {title}
         </h2>
       </div>
