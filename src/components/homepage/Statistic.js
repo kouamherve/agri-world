71328ee8432a1edf231e7566e8b8f654cb1cd7f5
Herve Kouam <kouamherve01@gmail.com>
2024-05-30 06:05:47 -0400

    🚧  F03: APP > Statistic > Add MD breakpoint

--- src/components/homepage/Statistic.js
+++ src/components/homepage/Statistic.js
@@ -3,7 +3,7 @@ import StatisticCard from "../../reusable-ui/StatisticCard";
 
 export default function Statistic() {
   return (
-    <div className=" bg-secondary py-14 grid grid-cols-2 gap-y-14">
+    <div className=" bg-secondary py-20 grid grid-cols-2 gap-y-14 md:gap-y-10">
       <StatisticCard number="130" title="custom products" />
       <StatisticCard number="40" title="our farms" />
       <StatisticCard number="312" title="happy customers" />
--- src/reusable-ui/StatisticCard.js
+++ src/reusable-ui/StatisticCard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function StatisticCard({ number, title }) {
   return (
-    <div className=" font-Montserrat mx-4 sm:mx-16">
+    <div className=" font-Montserrat mx-4 sm:mx-16 md:mx-10">
       <div className="text-center ">
-        <span className=" text-primary text-4xl font-extrabold">{number}</span>
-        <h2 className=" text-gray-800 mt-3 text-lg capitalize font-light sm:text-xl">
+        <span className=" text-primary text-4xl font-extrabold md:text-5xl">
+          {number}
+        </span>
+        <h2 className=" text-gray-800 mt-3 text-lg capitalize font-light md:text-xl">
           {title}
         </h2>
       </div>
