743f44aefba069860fc8aaa919b2fa8d829ce659
Herve Kouam <kouamherve01@gmail.com>
2024-05-30 06:02:51 -0400

    🚧  F03: APP > Navbar > Add MD breakpoint

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -10,28 +10,30 @@ export default function Navbar() {
   const [isShow, setIsShow] = useState(false);
 
   return (
-    <nav className="z-50 fixed top-0 left-0 right-0 px-4 py-5 bg-white font-Montserrat w-full flex flex-wrap items-center justify-between shadow overflow-x-hidden sm:px-16">
-      <div>
-        <Link to="#">
-          <h1 className=" text-3xl text-black font-extrabold font-sans sm:text-4xl ">
-            Agri<span className=" text-primary">world</span>
-          </h1>
-        </Link>
-      </div>
-      <div
-        onClick={() => setIsShow(!isShow)}
-        className="text-[34px] text-gray-800 cursor-pointer flex items-center gap-5 md:text-4xl lg:hidden"
-      >
-        <MdDarkMode className=" text-2xl md:text-[26px] p-0" />
-        {isShow ? <AiOutlineClose className=" text-[26px]" /> : <IoMenu />}
-      </div>
-      <Menu isShow={isShow} />
-      <div className=" hidden lg:flex items-center justify-between gap-4 ">
-        <Button className="px-10 py-3.5 text-nowrap bg-primary text-white">
-          Free Quote
-        </Button>
-        <MdDarkMode className=" text-2xl md:text-[26px] text-gray-600" />
-      </div>
-    </nav>
+    <div className=" relative">
+      <nav className="z-50 fixed top-0 left-0 right-0 px-4 py-4 bg-white font-Montserrat w-full flex flex-wrap items-center justify-between shadow overflow-x-hidden sm:px-16 md:px-8 md:py-5">
+        <div>
+          <Link to="#">
+            <h1 className=" text-3xl text-black font-extrabold font-sans sm:text-4xl">
+              Agri<span className=" text-primary">world</span>
+            </h1>
+          </Link>
+        </div>
+        <div
+          onClick={() => setIsShow(!isShow)}
+          className="text-[34px] text-gray-800 cursor-pointer flex items-center gap-5 md:text-4xl lg:hidden"
+        >
+          <MdDarkMode className=" text-2xl md:text-[26px] p-0" />
+          {isShow ? <AiOutlineClose className=" text-[26px]" /> : <IoMenu />}
+        </div>
+        <Menu isShow={isShow} />
+        <div className=" hidden lg:flex items-center justify-between gap-4 ">
+          <Button className="px-10 py-3.5 text-nowrap bg-primary text-white">
+            Free Quote
+          </Button>
+          <MdDarkMode className=" text-2xl md:text-[26px] text-gray-600" />
+        </div>
+      </nav>
+    </div>
   );
 }
