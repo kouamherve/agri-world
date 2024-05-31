01514376a3a2427988db2677fabe3c5a8a2b5bbd
Herve Kouam <kouamherve01@gmail.com>
2024-05-30 21:56:46 -0400

    ✨   F03: APP > Homepage > About > Add LG breakpoint

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -3,31 +3,31 @@ import Button from "../../reusable-ui/Button";
 
 export default function About() {
   return (
-    <div className=" bg-secondary py-14">
-      <div className=" mx-4 pb-8 font-Montserrat capitalize sm:mx-16 md:mx-8">
+    <div className=" bg-secondary py-14 lg:flex items-center lg:px-14 lg:py-24 gap-6">
+      <div className=" mx-4 pb-8 font-Montserrat capitalize sm:mx-16 md:mx-8 lg:mx-0 lg:w-1/2 lg:pb-0">
         <div>
-          <h4 className=" text-sm text-primary font-medium sm:text-base">
+          <h4 className=" text-sm mb-1 text-primary font-medium sm:text-base lg:text-sm">
             About us
           </h4>
-          <h1 className=" text-[26px] leading-snug font-bold sm:text-4xl">
-            A new way to invest <br className="hidden md:block" />
+          <h1 className=" text-[26px] leading-snug font-bold sm:text-4xl lg:text-[34px] lg:leading-snug lg:mt-1">
+            A new way to invest <br className="hidden md:block lg:hidden" />
             in agriculture
           </h1>
         </div>
-        <p className=" text-gray-600 mt-5 mb-8">
+        <p className=" text-[15px] text-gray-600 mt-4 mb-8 lg:text-base">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ad,
           omnis repellendus quidem pariatur eum velit corrupti voluptatum,
           minima aliquid suscipit enim dolorem perspiciatis rem molestiae fuga
           praesentium quo fugit deleniti quam eveniet. Veritatis, eius veniam
           soluta in placeat dolorem.
         </p>
-        <Button className="bg-primary text-white hover:text-primary border border-primary px-8 py-3.5 mb-4 text-base sm:px-9 md:px-10 ">
+        <Button className="bg-primary text-white hover:text-primary border border-primary px-8 py-3.5 mb-4 sm:px-9 md:px-10">
           Read more
         </Button>
       </div>
-      <div className=" mx-4 h-64 w-auto sm:mx-16 sm:h-96 md:mx-8 md:h-[510px]">
+      <div className=" relative mx-4 h-64 w-auto sm:mx-16 sm:h-96 md:mx-8 md:h-[510px] lg:w-1/2 lg:h-96 lg:mx-0">
         <img
-          className=" object-cover object-center w-full h-full rounded-lg"
+          className=" absolute inset-0 object-cover object-center w-full h-full rounded-lg"
           src="https://cdn.pixabay.com/photo/2021/05/03/13/32/greenhouse-6226263_1280.jpg"
           alt=""
         />
