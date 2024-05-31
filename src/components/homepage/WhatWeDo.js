ded17b1ede16588ff558da9ffad5cbc9708b057e
Herve Kouam <kouamherve01@gmail.com>
2024-05-30 21:57:09 -0400

    ✨   F03: APP > Homepage > WhatWeDo > Add LG breakpoint

--- src/components/homepage/WhatWeDo.js
+++ src/components/homepage/WhatWeDo.js
@@ -2,26 +2,26 @@ import Check from "../../reusable-ui/Check";
 
 export default function WhatWeDo() {
   return (
-    <div className=" pt-14 pb-14">
-      <div className=" mx-4 h-64 w-auto sm:mx-16 sm:h-96 md:mx-8 md:h-[510px]">
+    <div className=" py-14 lg:py-24 lg:mx-14 lg:flex items-center gap-6">
+      <div className=" mx-4 h-64 w-auto sm:mx-16 sm:h-96 md:mx-8 md:h-[510px] lg:h-96 lg:w-1/2 lg:mx-0">
         <img
           className=" object-cover object-center w-full h-full rounded-lg"
           src="https://cdn.pixabay.com/photo/2018/10/03/21/57/cabbage-3722498_1280.jpg"
           alt=""
         />
       </div>
-      <div className=" mx-4 pt-14 font-Montserrat capitalize sm:mx-16 md:mx-8">
+      <div className=" mx-4 pt-14 font-Montserrat capitalize sm:mx-16 md:mx-8 lg:w-1/2 lg:mx-0 lg:pt-0">
         <h4 className=" text-sm text-primary font-medium sm:text-base">
           What we do
         </h4>
-        <h1 className=" text-[26px] leading-8 font-bold mt-0 sm:text-4xl">
+        <h1 className=" text-[26px] leading-8 font-bold mt-0 sm:text-4xl lg:mt-1">
           What we offer for you
         </h1>
-        <p className=" text-gray-600 my-6">
+        <p className=" text-[15px] text-gray-600 my-6 lg:my-7">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, dicta
           laboriosam. Deserunt fugiat accusantium explicabo excepturi.
         </p>
-        <div className=" space-y-4 md:space-y-6">
+        <div className=" space-y-4 md:space-y-6 lg:space-y-2">
           <Check service="weed control" />
           <Check service="pest control" />
           <Check service="grass sedding" />
--- src/reusable-ui/Check.js
+++ src/reusable-ui/Check.js
@@ -2,11 +2,11 @@ import { GiCheckMark } from "react-icons/gi";
 
 export default function Check({ service }) {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 lg:text-gray-800/90">
       <div className=" bg-secondary p-2 size-6 rounded-full md:size-8">
         <GiCheckMark className=" text-[10px] md:text-[14px]" />
       </div>
-      <span className=" text-base font-medium">{service}</span>
+      <span className="  text-base font-medium lg:font-bold">{service}</span>
     </div>
   );
 }
