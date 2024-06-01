5e2e0165387825f7f616d9318dfe290d1e0df690
Herve Kouam <kouamherve01@gmail.com>
2024-06-01 19:30:47 -0400

    ✨ F03: APP > Homepage > BeforeAndAfter > Add XL breakpoint

--- src/components/homepage/BeforeAndAfter.js
+++ src/components/homepage/BeforeAndAfter.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function BeforeAndAfter() {
   return (
-    <div className=" bg-secondary py-20 space-y-6 sm:space-y-4 md:flex md:space-y-0 lg:py-24 lg:px-14">
-      <div className=" relative mx-4 h-64 sm:mx-16 sm:h-[270px] md:h-72 md:mr-2 lg:mx-0 lg:h-96">
+    <div className=" bg-secondary py-20 space-y-6 sm:space-y-4 md:flex md:space-y-0 lg:py-24 lg:px-14 xl:px-36">
+      <div className=" relative mx-4 h-64 sm:mx-16 sm:h-[270px] md:h-72 md:mr-2 lg:mx-0 lg:h-96 xl:h-[460px]">
         <img
           className=" object-cover object-center w-full h-full rounded-t-lg md:rounded-l-lg md:rounded-tr-none"
           src="https://cdn.pixabay.com/photo/2015/10/06/00/31/green-973596_1280.jpg"
@@ -13,7 +13,7 @@ export default function BeforeAndAfter() {
           Before
         </span>
       </div>
-      <div className=" relative mx-4 h-64 sm:mx-16 sm:h-[270px] md:h-72 md:ml-2 lg:mx-0 lg:h-96 ">
+      <div className=" relative mx-4 h-64 sm:mx-16 sm:h-[270px] md:h-72 md:ml-2 lg:mx-0 lg:h-96 xl:h-[460px] ">
         <img
           className=" object-cover object-center w-full h-full rounded-b-lg md:rounded-r-lg md:rounded-bl-none"
           src="https://cdn.pixabay.com/photo/2022/04/04/07/58/seedlings-7110591_1280.jpg"
