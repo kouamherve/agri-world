8567d9720ed22ae94a0049762cb3118a31aa60cb
Herve Kouam <kouamherve01@gmail.com>
2024-06-01 19:31:08 -0400

    ✨ F03: APP > Homepage > HealthyFood > Add XL breakpoint

--- src/components/homepage/HealthyFood.js
+++ src/components/homepage/HealthyFood.js
@@ -3,12 +3,12 @@ import HealthyFoodCard from "../../reusable-ui/HealthyFoodCard";
 
 export default function HealthyFood() {
   return (
-    <div className=" py-14 sm:py-16 md:py-20 lg:py-24">
-      <div className=" mx-4 font-Montserrat capitalize sm:mx-14 sm:mb-8 md:mx-20 md:mb-10 lg:mx-14 lg:mb-12">
+    <div className="px-4 py-14 sm:py-16 sm:px-14 md:px-20 md:py-20 lg:px-14 lg:py-24 xl:px-36">
+      <div className=" font-Montserrat capitalize sm:mb-8 0 md:mb-10 lg:mb-12">
         <h3 className=" text-sm text-primary font-medium sm:text-base">
           Healthy food
         </h3>
-        <h1 className=" text-[26px] leading-snug font-bold sm:text-3xl md:text-4xl lg:leading-snug">
+        <h1 className=" text-[26px] text-black/85 leading-snug font-bold sm:text-3xl md:text-4xl lg:leading-snug xl:text-[40px]">
           We provide high-quality <br />
           organic products
         </h1>
@@ -16,7 +16,7 @@ export default function HealthyFood() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, nobis!
         </p>
       </div>
-      <div className="capitalize space-y-14 gap-8 md:space-y-0 md:flex md:gap-6 md:mx-20 lg:mx-14">
+      <div className="capitalize space-y-14 gap-8 md:space-y-0 md:flex md:gap-6 xl:gap-8 xl:mt-6">
         <HealthyFoodCard
           title="industry oriented"
           image="https://cdn.pixabay.com/photo/2017/03/13/12/16/greenhouse-2139526_1280.jpg"
