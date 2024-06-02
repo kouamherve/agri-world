32837e45e1999be97fa28c0d376be132de053c46
Herve Kouam <kouamherve01@gmail.com>
2024-06-01 20:03:09 -0400

    ✨  F03: App > Homepage > Statistic > Add 2XL breakpoint

--- src/components/homepage/Statistic.js
+++ src/components/homepage/Statistic.js
@@ -3,7 +3,7 @@ import StatisticCard from "../../reusable-ui/StatisticCard";
 
 export default function Statistic() {
   return (
-    <div className=" bg-secondary py-16 grid grid-cols-2 gap-y-14 px-4 sm:px-14 md:gap-x-12 md:flex md:items-center md:justify-center xl:px-36 xl:py-28 xl:justify-between">
+    <div className=" bg-secondary py-16 grid grid-cols-2 gap-y-14 px-4 sm:px-14 md:gap-x-12 md:flex md:items-center md:justify-center xl:px-36 xl:py-28 xl:justify-between 2xl:px-[710px]">
       <StatisticCard number="130" title="custom products" />
       <StatisticCard number="40" title="our farms" />
       <StatisticCard number="312" title="happy customers" />
