36109931aae2bf441d20505066e5c54a2ab1bd47
Herve Kouam <kouamherve01@gmail.com>
2024-06-01 20:01:44 -0400

    ✨  F03: App > Homepage > HomapageCard > Add 2XL breakpoint

--- src/components/homepage/HomepageCard.js
+++ src/components/homepage/HomepageCard.js
@@ -9,18 +9,18 @@ export default function HomepageCard({ slide }) {
         src={slide.imageUrl}
         alt={slide.title}
       />
-      <div className="absolute inset-0 mx-4 flex flex-col justify-center items-start capitalize font-Montserrat sm:mx-14 md:mx-20 lg:mx-14 lg:mt-4 xl:mx-36 xl:mt-0 xl:-top-32">
+      <div className="absolute inset-0 mx-4 flex flex-col justify-center items-start capitalize font-Montserrat sm:mx-14 md:mx-20 lg:mx-14 lg:mt-4 xl:mx-36 xl:mt-0 xl:-top-32 2xl:mx-[710px]">
         <h3 className="text-white text-sm mb-2 font-light sm:text-base md:text-lg md:mb-0 xl:text-lg">
           {slide.subTitle}
         </h3>
-        <p className="text-white text-[32px] leading-snug font-extrabold mb-6 sm:text-[40px] md:text-5xl md:leading-snug lg:mb-8 lg:max-w-2xl xl:text-7xl xl:max-x-5xl xl:mt-2 xl:leading-tight">
+        <p className="text-white text-[31px] font-extrabold mb-5 sm:text-[40px] md:text-5xl md:leading-snug lg:mb-8 lg:max-w-2xl xl:text-7xl xl:max-w-3xl xl:mt-2 xl:leading-tight 2xl:text-[74px]">
           {slide.title}
         </p>
         <Button className="bg-white mt-1 text-primary hover:text-white text-base border border-white px-5 py-3 sm:px-8 md:px-10 md:py-3.5">
           Read more
         </Button>
       </div>
-      <div className=" absolute my-4 mx-2 bottom-0 right-0 flex items-center justify-center sm:mb-8 sm:mx-6 xl:mb-20">
+      <div className=" absolute my-4 mx-2 bottom-0 right-0 flex items-center justify-center sm:mb-8 sm:mx-6 xl:mb-20 2xl:mb-24 2xl:mx-10">
         <HomepageMiniCard
           title={slide.titleMini1}
           image={slide.imageUrlMini1}
--- src/components/homepage/HomepageMiniCard.js
+++ src/components/homepage/HomepageMiniCard.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function HomepageMiniCard({ image, title }) {
   return (
-    <div className="h-14 w-20 bg-white ml-5 rounded-lg font-Montserrat flex flex-col items-center justify-center sm:w-[136px] sm:h-40 md:w-36 lg:w-44 lg:h-[196px] xl:w-72 ">
-      <div className="w-full p-2 h-full sm:p-3 sm:h-2/5 md:h-1/2 xl:h-3/4 xl:p-4">
+    <div className="h-14 w-20 bg-white ml-5 rounded-lg font-Montserrat flex flex-col items-center justify-center sm:w-[136px] sm:h-40 md:w-36 lg:w-44 lg:h-[196px] xl:w-72 2xl:w-[500px] 2xl:h-80 ">
+      <div className="w-full p-2 h-full sm:p-3 sm:h-2/5 md:h-1/2 xl:h-3/4 xl:p-4 2xl:h-4/5">
         <img
           className=" object-cover object-bottom w-full h-full rounded-lg sm:object-center "
           src={image}
           alt=""
         />
       </div>
-      <div className=" hidden sm:block h-3/5 md:h-1/2 xl:h-1/4 ">
+      <div className=" hidden sm:block h-3/5 md:h-1/2 xl:h-1/4 2xl:h-1/5 ">
         <h2 className=" text-[15px] text-center text-gray-900 font-extrabold mt-1 lg:text-xl xl:text-[18px]">
           {title}
         </h2>
